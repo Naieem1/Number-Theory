3ebceab3c660f33beda9c67746debdaa7fa43840
MD. Naieem Al Zaman <naieemalzaman@gmail.com>
2018-03-06 15:29:37 +0600

    Update SieveOfEratosthenes_02.cpp

--- Prime Number/SieveOfEratosthenes_02.cpp
+++ Prime Number/SieveOfEratosthenes_02.cpp
@@ -23,12 +23,12 @@ int main()
             }
         }
     }
-    for(int i=2; i<n; i++)
+    printf("2 ");
+    for(int i=3; i<=n; i+=2)
     {
         if(arr[i])
         {
             printf("%d ",i);
         }
     }
-    return 0;
 }
